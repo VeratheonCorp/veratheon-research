ac78cd50c945d11a5afc729047bb62925110eda9
Ryan Fritts <ryan.fritts@live.com>
2025-08-16 18:50:30 -0400

    chore: remove unused documentation and cleanup scripts

--- delete_useless_python_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Delete all __init__.py files
-find ./src -type f -name '__init__.py' -delete
-
-# Delete all __pycache__ directories and their contents
-find ./src -type d -name '__pycache__' -exec rm -rf {} +
